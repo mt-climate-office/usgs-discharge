44b6da430d88346882a4d528285b79005766edb6
Zach Hoylman <zhoylman@gmail.com>
2024-05-22 09:53:35 -0600

    updated plot

--- R/plot.R
+++ R/plot.R
@@ -107,8 +107,8 @@ make_climatology_plot <- function(data, STANAME, STAID, ...) {
     ggplot2::theme_bw(base_size = 14) +
     ggplot2::labs(x='', y='Discharge [cfs]',
          fill=glue::glue("Past\nConditions\n{this_year-30}-{this_year}"),
-         title=glue::glue("USGS Gauge {STAID}\n{STANAME}")) +
-    ggplot2::theme(plot.title = ggplot2::element_text(face = "bold", hjust = 0.0)) +
+         title=glue::glue("USGS Gauge {STAID} (",max(data$date),")\n{STANAME}")) +
+    ggplot2::theme(plot.title = ggplot2::element_text(face = "bold", hjust = 0.5)) +
     ggplot2::scale_y_log10()
 
   if ("out_dir" %in% names(other_args)) {
